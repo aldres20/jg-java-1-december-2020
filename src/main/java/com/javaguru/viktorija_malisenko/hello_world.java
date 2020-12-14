9445deb13c513af163afe4d5e97847c5f8ea07e5
Viktorija007 <viktorija.malisenko@gmail.com>
2020-12-14 21:35:26 +0200

    first_commit

---
+++ src/main/java/com/javaguru/viktorija_malisenko/hello_world.java
@@ -0,0 +1,9 @@
+package com.javaguru.viktorija_malisenko;
+
+public class hello_world {
+
+        public static void main(String[] args) {
+            System.out.println("hello_world");
+        }
+    }
+
