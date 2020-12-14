f20f987be31be305eb76902d02b272ab9958c9ec
Ruslans Tufetulovs <ruslans.tufetulovs@gmail.com>
2020-12-14 22:00:54 +0200

    fixes

--- src/main/java/com/javaguru/Viktorija_Tarane_student/HelloWorld.java
+++ src/main/java/com/javaguru/Viktorija_Tarane_student/HelloWorld.java
@@ -1,8 +1,7 @@
 package com.javaguru.Viktorija_Tarane_student;
 
 public class HelloWorld {
-}
-public static void main(String[] args) {
+    public static void main(String[] args) {
         System.out.println("Hello World");
     }
 }
