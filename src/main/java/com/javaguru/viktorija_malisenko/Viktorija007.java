0e6d1e4a68bedbad8e510a9c37f1fad15ff4f3a9
Viktorija007 <viktorija.malisenko@gmail.com>
2020-12-14 22:09:16 +0200

    first-commit

--- src/main/java/com/javaguru/viktorija_malisenko/Viktorija007.java
+++ src/main/java/com/javaguru/viktorija_malisenko/Viktorija007.java
@@ -1,7 +1,7 @@
 package com.javaguru.viktorija_malisenko;
 
-public class HelloWorld {
+public class Viktorija007 {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Viktorija007");
     }
 }
