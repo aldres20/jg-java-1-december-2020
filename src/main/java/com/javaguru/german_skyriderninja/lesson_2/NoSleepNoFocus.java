784c9427366f413e8b15447f33e16bdac3c5c30f
Strix <ge.dm.gal@gmail.com>
2020-12-15 14:20:15 +0200

    Full commit German

--- src/main/java/com/javaguru/german_skyriderninja/lesson_2/NoSleepNoFocus.java
+++ src/main/java/com/javaguru/german_skyriderninja/lesson_2/NoSleepNoFocus.java
@@ -3,8 +3,8 @@ package com.javaguru.german_skyriderninja.lesson_2;
 public class NoSleepNoFocus {
 
     public static void main(String[] args) {
-         System.out.println("I wanna coffee");
-         System.out.println("So bad");
+         System.out.println("I wanna coffee!");
+         System.out.println("So bad!");
     }
 }
 //Task_24
