11d6913dd85d75cf0fba4eed12ab3c9a134c4d51
Viktorija Tarane <viktorija.makaruka@gmail.com>
2020-12-15 17:54:24 +0200

    Viktorija_Tarane_1lesson

--- src/main/java/com/javaguru/Viktorija_Tarane_student/Task11.java
+++ src/main/java/com/javaguru/Viktorija_Tarane_student/Task11.java
@@ -3,9 +3,9 @@ package com.javaguru.Viktorija_Tarane_student;
 public class Task11 {
     public static void main(String[] args) {
 
-        1 int firstnumber = 74;
-        2 int secondnumber = 36;
-        3 int sumresult = firstnumber + secondnumber;
-        4 System.out.println(sumresult);
+        int firstnumber = 74;
+        int secondnumber = 36;
+        int sumresult = firstnumber + secondnumber;
+        System.out.println(sumresult);
     }
 }
