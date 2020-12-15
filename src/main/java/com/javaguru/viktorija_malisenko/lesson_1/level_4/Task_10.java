7127ea12a83b580f89ce32740e0baea1d0e13fcc
Viktorija Malisenko <viktorija.malisenko@gmail.com>
2020-12-15 17:55:08 +0200

    first-commit

---
+++ src/main/java/com/javaguru/viktorija_malisenko/lesson_1/level_4/Task_10.java
@@ -0,0 +1,8 @@
+package com.javaguru.viktorija_malisenko.lesson_1.level_4;
+
+
+public class Task_10{
+    public static void main(String[] args) {
+        System.out.println("Hello Alexandra Abramova");
+    }
+}
