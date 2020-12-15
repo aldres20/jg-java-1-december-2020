1e0cbddead9391391e043e76a1f924c5f6671c44
Strix <skyriderjava@gmail.com>
2020-12-15 21:56:37 +0200

    Full commit German

--- src/main/java/com/javaguru/vinsents/SiegHeil.java
+++ src/main/java/com/javaguru/vinsents/SiegHeil.java
@@ -1,5 +1,14 @@
 package com.javaguru.vinsents;
 
-public class SiegHeil { public static void main(String[] args) {
-    System.out.println("SiegHeil");
+import com.javaguru.teacher.codereview.CodeReviewComment;
+
+@CodeReviewComment(teacher = "необходимо убедиться, что код запускается перед тем как сделать commit")
+@CodeReviewComment(teacher = "Название класса и название файла должны совпадать")
+public class SiegHeil {
+
+    /*
+    public static void main(String[] args) { System.out.println("Sieg Heil);}
+    }
+     */
+
 }
