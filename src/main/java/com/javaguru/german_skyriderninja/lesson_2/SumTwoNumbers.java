e30bba0780f5ab29b0b57c8669dfb06b8991030f
Strix <skyriderjava@gmail.com>
2020-12-15 17:25:17 +0200

    Full commit German

--- src/main/java/com/javaguru/german_skyriderninja/lesson_2/SumTwoNumbers.java
+++ src/main/java/com/javaguru/german_skyriderninja/lesson_2/SumTwoNumbers.java
@@ -6,13 +6,10 @@ public class SumTwoNumbers {
 
         int numberOne = 10;
         int numberTwo = 20;
+        int sum = numberOne + numberTwo;
 
         System.out.println("Number 1 = " + numberOne);
         System.out.println("Number 2 = " + numberTwo);
-
-
-        int sum = numberOne + numberTwo;
-
         System.out.println("Sum = " + sum);
     }
 }
