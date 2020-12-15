66466c427da3acc25cdbb91cfe035a2e8aa66ea7
Your Name <you@example.com>
2020-12-15 16:23:21 +0200

    Andrejs Jukšs

---
+++ src/main/java/com/javaguru/andrejs_jukss/lesson_1_level_4_task_10.java
@@ -0,0 +1,11 @@
+package com.javaguru.andrejs_jukss;
+
+public class lesson_1_level_4_task_10 {
+    public static void main(String[] args){
+        String a = "Andrejs ";
+        String b = "Jukss";
+        System.out.println(a+b);
+
+
+    }
+}
---
+++ src/main/java/com/javaguru/andrejs_jukss/lesson_1_level_4_task_11.java
@@ -0,0 +1,9 @@
+package com.javaguru.andrejs_jukss;
+
+public class lesson_1_level_4_task_11 {
+    public static void main (String [] args){
+        int a = 74;
+        int b = 36;
+        System.out.println(a+b);
+    }
+}
