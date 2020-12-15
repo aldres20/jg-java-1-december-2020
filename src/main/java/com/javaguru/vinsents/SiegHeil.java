96769a3a64ea45a9846a4dab3702adbb6435e37a
naidanodoklis <bmw_f650@inbox.lv>
2020-12-15 21:49:04 +0200

    first-commit

--- src/main/java/com/javaguru/vinsents/SiegHeil.java
+++ src/main/java/com/javaguru/vinsents/SiegHeil.java
@@ -1,14 +1,5 @@
 package com.javaguru.vinsents;
 
-import com.javaguru.teacher.codereview.CodeReviewComment;
-
-@CodeReviewComment(teacher = "необходимо убедиться, что код запускается перед тем как сделать commit")
-@CodeReviewComment(teacher = "Название класса и название файла должны совпадать")
-public class SiegHeil {
-
-    /*
-    public static void main(String[] args) { System.out.println("Sieg Heil);}
-    }
-     */
-
+public class SiegHeil { public static void main(String[] args) {
+    System.out.println("SiegHeil");
 }
