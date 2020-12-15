80847d230f24ca67407bb76d59eb16952ef2e0c2
Strix <skyriderjava@gmail.com>
2020-12-15 19:45:49 +0200

    Full commit German

---
+++ src/main/java/com/javaguru/german_skyriderninja/lesson_1/SumDivideNum.java
@@ -0,0 +1,17 @@
+package com.javaguru.german_skyriderninja.lesson_1;
+
+public class SumDivideNum {
+
+    public static void main(String[] args) {
+
+        int num1 = 74;
+        int num2 = 36;
+        int num3 = 50;
+        int num4 = 3;
+
+        System.out.println(num1 + num2);
+        System.out.println(num3 / num4);
+    }
+}
+//Ещё один вариант, самы простой, наверное.
+//Task_11
