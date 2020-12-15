d7be454e27303760cdba94f1837760d7fcc9db96
Strix <skyriderjava@gmail.com>
2020-12-15 14:36:39 +0200

    Full commit German

--- src/main/java/com/javaguru/german_skyriderninja/lesson_2/HelloPrint.java
+++ src/main/java/com/javaguru/german_skyriderninja/lesson_2/HelloPrint.java
@@ -3,7 +3,7 @@ package com.javaguru.german_skyriderninja.lesson_2;
 public class HelloPrint {
 
     public static void main(String[] args) {
-        System.out.print("Hello World");
+        System.out.print("Hello Print");
     }
 }
 //Task_15
