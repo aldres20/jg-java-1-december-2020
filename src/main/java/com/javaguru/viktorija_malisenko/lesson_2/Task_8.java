acdfa0eac5e15b2963f050bf1019343b0e34b3e1
Viktorija Malisenko <viktorija.malisenko@gmail.com>
2020-12-16 19:53:35 +0200

    first-commit

---
+++ src/main/java/com/javaguru/viktorija_malisenko/lesson_2/Task_8.java
@@ -0,0 +1,15 @@
+package com.javaguru.viktorija_malisenko.lesson_2;
+
+import java.util.Scanner;
+
+public class Task_8 {
+    public static void main(String [] args) {
+        Scanner scn = new Scanner(System.in);
+        System.out.println(" Введите свое имя ");
+        String name = scn.nextLine();
+
+        System.out.println("Hello " + name);
+
+
+    }
+}
