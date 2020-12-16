e1c1809b58b43e9f9583415d464d8a5eb292e420
aldres20 <aleksandrs.drozdovs@gmail.vom>
2020-12-16 20:52:21 +0200

    Lesson2 Task 1 and 2 from Aleks

---
+++ src/main/java/com/javaguru/aleksandrs_drozdovs/lesson_2/Task_1.java
@@ -0,0 +1,22 @@
+package com.javaguru.aleksandrs_drozdovs.lesson_2;
+
+
+import java.util.Scanner;
+
+public class Task_1 {
+
+    public static void main (String[] args){
+
+        Scanner input = new Scanner (System.in);
+        System.out.println("Input the first number: ");
+        int a = input.nextInt();
+        System.out.println("Input the second number: ");
+        int b = input.nextInt();
+        System.out.println(a + " + " + b + " = " + (a + b));
+        System.out.println(a + " - " + b + " = " + (a - b));
+        System.out.println(a + " * " + b + " = " + (a * b));
+        System.out.println(a + " / " + b + " = " + (a / b));
+
+
+    }
+}
