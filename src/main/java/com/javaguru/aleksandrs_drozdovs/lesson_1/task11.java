de2d7cf4ec5ab50cb47ccbb5d200182f13ec76f2
aldres20 <aleksandrs.drozdovs@gmail.vom>
2020-12-16 20:31:48 +0200

    Task 10 and 11 from Aleks

---
+++ src/main/java/com/javaguru/aleksandrs_drozdovs/lesson_1/task11.java
@@ -0,0 +1,27 @@
+package com.javaguru.aleksandrs_drozdovs.lesson_1;
+
+public class task11 {
+
+    public static void main (String[] args){
+
+        int a=74;
+        int b=36;
+        int sumdata = a + b;
+        System.out.println("Test Data:");
+        System.out.println(a + " + " + b);
+        System.out.println("Expected Output :");
+        System.out.println(sumdata);
+
+        System.out.println();
+
+        int c=50;
+        int d=3;
+        int dividedata = c / d;
+        System.out.println("Test Data:");
+        System.out.println(c + " / " + d);
+        System.out.println("Expected Output :");
+        System.out.println(dividedata);
+
+
+    }
+}
