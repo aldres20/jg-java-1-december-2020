4ed9fc09df9df530412849db3d809787bbb91ab0
Viktorija Malisenko <viktorija.malisenko@gmail.com>
2020-12-17 14:07:37 +0200

    first-commit

---
+++ src/main/java/com/javaguru/viktorija_malisenko/lesson_2/BankAccount.java
@@ -0,0 +1,8 @@
+package com.javaguru.viktorija_malisenko.lesson_2;
+
+public class BankAccount {
+        public static void main(String[] args){
+
+            System.out.println("BankAccount is corrected");
+        }
+}
--- src/main/java/com/javaguru/viktorija_malisenko/lesson_2/Task_4.java
+++
@@ -1,8 +0,0 @@
-package com.javaguru.viktorija_malisenko.lesson_2;
-
-public class Task_4 {
-
-    public static void main(String[] args){
-        System.out.println("Class Phone is corrected ");
-    }
-}
