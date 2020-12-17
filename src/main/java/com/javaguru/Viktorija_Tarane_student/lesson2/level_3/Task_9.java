31d8cdb51bb1a34e772d45753cca029a25d09161
Viktorija Tarane <viktorija.makaruka@gmail.com>
2020-12-17 13:27:14 +0200

    Viktorija_Tarane_2lesson

---
+++ src/main/java/com/javaguru/Viktorija_Tarane_student/lesson2/level_3/Task_9.java
@@ -0,0 +1,16 @@
+package com.javaguru.Viktorija_Tarane_student.lesson2.level_3;
+
+import java.util.Scanner;
+
+public class Task_9 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a;
+        System.out.print("Input a number: ");
+        a = scan.nextInt();
+        for ( int b = 1; b <= 10; b++)
+
+        System.out.println("Expected Output :" + a * b);
+
+    }
+}
