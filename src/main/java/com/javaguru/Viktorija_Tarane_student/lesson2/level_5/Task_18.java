5aed173cd0c8833e86c241f023b77f941e596194
Viktorija Tarane <viktorija.makaruka@gmail.com>
2020-12-17 15:11:43 +0200

    Viktorija_Tarane_2lesson

---
+++ src/main/java/com/javaguru/Viktorija_Tarane_student/lesson2/level_5/Task_18.java
@@ -0,0 +1,9 @@
+package com.javaguru.Viktorija_Tarane_student.lesson2.level_5;
+
+public class Task_18 {
+
+        public static void main(String[] args) {
+            System.out.println("Sorry, I have an errors.");
+            System.out.println("Please fix me!");
+        }
+}
