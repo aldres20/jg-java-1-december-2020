31c76f945944d6e3b9b1888a6ac783c756f708f7
Viktorija Malisenko <viktorija.malisenko@gmail.com>
2020-12-17 14:06:05 +0200

    first-commit

---
+++ src/main/java/com/javaguru/viktorija_malisenko/lesson_2/Phone.java
@@ -0,0 +1,7 @@
+package com.javaguru.viktorija_malisenko.lesson_2;
+
+public class Phone {
+    public static void main(String[] args) {
+        System.out.println("Class Phone is corrected ");
+    }
+}
