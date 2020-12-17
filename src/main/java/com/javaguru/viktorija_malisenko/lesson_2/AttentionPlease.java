e72c089adb43b337f2ae9333e6caaf62ada87a66
Viktorija Malisenko <viktorija.malisenko@gmail.com>
2020-12-17 14:29:01 +0200

    first-commit

---
+++ src/main/java/com/javaguru/viktorija_malisenko/lesson_2/AttentionPlease.java
@@ -0,0 +1,11 @@
+package com.javaguru.viktorija_malisenko.lesson_2;
+
+public class AttentionPlease {
+
+     public static void main(String[] args) {
+     System.out.println("Code outside the method will not compile!");
+
+
+    }
+
+}
