eeb4ce5708d7ed1c62bdc1c6517f8a878df70d9a
Viktorija Tarane <viktorija.makaruka@gmail.com>
2020-12-17 10:51:21 +0200

    Viktorija_Tarane_2lesson_Book

---
+++ src/main/java/com/javaguru/Viktorija_Tarane_student/lesson2/Task_2.java
@@ -0,0 +1,30 @@
+package com.javaguru.Viktorija_Tarane_student.lesson2;
+
+import java.util.Scanner;
+
+public class Task_2 {
+    public static void main(String args[]){
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Input a first number: ");
+        double firstDoubleNumber = scan.nextDouble();
+        System.out.print("Input a second number: ");
+        double secondDoubleNumber = scan.nextDouble();
+
+        double summa = firstDoubleNumber + secondDoubleNumber;
+        System.out.print("Sum: "+ summa);
+
+        double subtraction = firstDoubleNumber - secondDoubleNumber;
+        System.out.print("Sub: "+ subtraction);
+
+        double division = firstDoubleNumber / secondDoubleNumber;
+        System.out.print("Div: "+ division);
+
+        double multiplication = firstDoubleNumber * secondDoubleNumber;
+        System.out.print("Mult: "+ multiplication);
+
+
+    }
+}
+
+
+
