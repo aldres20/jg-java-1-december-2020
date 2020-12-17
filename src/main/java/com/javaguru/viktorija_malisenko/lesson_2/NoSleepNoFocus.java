ae46232c56a895204bc5fc8f240805896dfc708d
Viktorija Malisenko <viktorija.malisenko@gmail.com>
2020-12-17 14:31:51 +0200

    first-commit

---
+++ src/main/java/com/javaguru/viktorija_malisenko/lesson_2/NoSleepNoFocus.java
@@ -0,0 +1,12 @@
+package com.javaguru.viktorija_malisenko.lesson_2;
+
+public class NoSleepNoFocus {
+        public static void main(String[] args) {
+            System.out.println("I wanna coffee");
+            System.out.println("So bad");
+
+
+
+        }
+
+}
