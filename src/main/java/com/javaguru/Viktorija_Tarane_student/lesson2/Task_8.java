f3c6202fd87f5439f72f7f7cead751f02d165e36
Viktorija Tarane <viktorija.makaruka@gmail.com>
2020-12-17 12:20:12 +0200

    Viktorija_Tarane_2lesson

---
+++ src/main/java/com/javaguru/Viktorija_Tarane_student/lesson2/Task_8.java
@@ -0,0 +1,13 @@
+package com.javaguru.Viktorija_Tarane_student.lesson2;
+
+import java.util.Scanner;
+
+public class Task_8 {
+    public static void main(String[] args) {
+        System.out.println("Введите Ваше имя пользователя и нажмите <Enter>:");
+        Scanner scan = new Scanner(System.in);
+        String name = scan.next();
+        System.out.println("Привет, " + name);
+        scan.close();
+    }
+}
