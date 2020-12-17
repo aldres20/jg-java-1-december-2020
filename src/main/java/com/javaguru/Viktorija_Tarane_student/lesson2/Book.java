b99f7a72322b980f95cc694c8ee77ed94f092930
Viktorija Tarane <viktorija.makaruka@gmail.com>
2020-12-17 10:27:11 +0200

    Viktorija_Tarane_2lesson_Book

---
+++ src/main/java/com/javaguru/Viktorija_Tarane_student/lesson2/Book.java
@@ -0,0 +1,4 @@
+package com.javaguru.Viktorija_Tarane_student.lesson2;
+
+public class Book {
+}
---
+++ src/main/java/com/javaguru/Viktorija_Tarane_student/lesson2/Task_3.java
@@ -0,0 +1,6 @@
+package com.javaguru.Viktorija_Tarane_student.lesson2;
+
+public class Task_3{
+    public static void main(String[] args){
+        }
+}
