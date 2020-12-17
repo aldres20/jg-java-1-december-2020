0a05037a319e4e056cab612241add7c4cb844fde
Viktorija Malisenko <viktorija.malisenko@gmail.com>
2020-12-17 14:08:55 +0200

    first-commit

---
+++ src/main/java/com/javaguru/viktorija_malisenko/lesson_2/Turtle.java
@@ -0,0 +1,7 @@
+package com.javaguru.viktorija_malisenko.lesson_2;
+
+public class Turtle {
+        public static void main(String[] args){
+        System.out.println("Class Turtle is corrected");
+    }
+}
