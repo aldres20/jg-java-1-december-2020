24cde52a888e8e9d8ee9f6fb1697dc8d9ed18dc3
Viktorija Tarane <viktorija.makaruka@gmail.com>
2020-12-17 11:52:45 +0200

    Viktorija_Tarane_2lesson_LOGIN

--- src/main/java/com/javaguru/Viktorija_Tarane_student/lesson2/LoginService.java
+++ src/main/java/com/javaguru/Viktorija_Tarane_student/lesson2/LoginService.java
@@ -1,7 +1,7 @@
 package com.javaguru.Viktorija_Tarane_student.lesson2;
 
 public class LoginService {
-    public void main(String[] args) {
+    public static void main(String[] args) {
         System.out.println("You can login!");
     }
 }
